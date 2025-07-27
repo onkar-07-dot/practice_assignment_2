334baa70f37ff4f2b5e45b62b92799a6aed1b560
[onkar-07-dot] <[onkarnanavare007@gmail.com]>
2025-07-28 00:04:04 +0530

    This is the updated code from M2

--- prime_number.js
+++ prime_number.js
@@ -1,12 +1,12 @@
  
 
- function prime(num){
+ function prime(number){
 
-    for(let i=2;i<num;i++){
+    for(let i=2;i<number;i++){
 
-        if(num%i!=0){
+        if(number%i!=0){
 
-            return true;
+           return true;
         }
         else{
 
@@ -17,3 +17,7 @@
 }
 
 console.log(prime(97));
+console.log(prime(100));
+console.log(prime(7));
+console.log(prime(9));
+console.log(prime(117));
