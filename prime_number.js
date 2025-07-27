79437ff6f50f912b54a67a6ddd091eb46d9de8b6
[onkar-07-dot] <[onkarnanavare007@gmail.com]>
2025-07-28 00:33:47 +0530

    this is fixed code by M1

--- prime_number.js
+++ prime_number.js
@@ -4,16 +4,17 @@
 
     for(let i=2;i<num;i++){
 
-        if(num%i!=0){
-
-            return true;
-        }
-        else{
+        if(num%i==0){
 
             return false;
         }
+    }
+        return true;
     }    
 
-}
+
 
 console.log(prime(97));
+console.log(prime(33));
+console.log(prime(17));
+console.log(prime(27));
